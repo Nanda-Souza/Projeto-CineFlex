f17bae248a867cadf868bb1fc26cb62c1b971c2c
Nanda-Souza <fesouoli@gmail.com>
2022-12-07 17:17:58 -0300

    Layout Inicial

---
+++ src/App.js
@@ -0,0 +1,24 @@
+
+function App() {
+
+  return (
+    <div className="screen-container"> 
+
+      <div className="header"> 
+      <h1>CINEFLEX</h1>
+      </div>
+
+      <div className="title-movie">
+      <p className="text">Selecione o filme</p>
+      </div>
+
+
+      <div className="footer"> 
+        <h1>Enola Holmes</h1>
+        </div>
+  
+  </div>
+  );
+}
+
+export default App;
